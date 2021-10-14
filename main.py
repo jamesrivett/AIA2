aba4c87f3423bf3d34c78ed283471da2b66730a9
James Rivett <jrivett@students.kennesaw.edu>
2021-10-14 13:39:34 -0400

    fillInit() now uses distinct values

--- main.py
+++ main.py
@@ -2,10 +2,28 @@ import random
 
 #globals
 goal = [[1, 2, 3],[8, 0, 4],[7, 6, 5]]
-init = []
+init = [[0, 0, 0],[0, 0, 0],[0, 0, 0]]
 
 def fillInit():
-    init = [[for i in range(3)] random.() for j in range(3)]
+    used = []
+    for i in range(3):
+        for j in range(3):
+            while True:
+                cont = False
+                num = random.randint(0, 8)
+                print(used)
+                print(num)
+
+                for element in used:
+                    if element == num:
+                        cont = True
+                        break
+                if cont == True: continue
+
+                init[i][j] = num
+                used.append(num)
+                break
+
 #main
 def main():
     fillInit()
