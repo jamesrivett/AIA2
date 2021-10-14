9b63aefdecc5fc2ec2772bb353ce41eb2c67e93a
James Rivett <jrivett@students.kennesaw.edu>
2021-10-14 16:10:55 -0400

    Distance Calculation added

--- main.py
+++ main.py
@@ -23,11 +23,61 @@ def fillInit():
                 used.append(num)
                 break
 
+def calculateDistance(num):
+    numpos = []
+    goalpos = []
+
+    #calculate num's position in init array
+    for i in range(3):
+        for j in range(3):
+            if init[i][j] == num:
+                numpos = [i, j]
+
+    #switch statement for goal positions
+    if num == 0:
+        goalpos = [1, 1]
+    if num == 1:
+        goalpos = [0, 0]
+    if num == 2:
+        goalpos = [0, 1]
+    if num == 3:
+        goalpos = [0, 2]
+    if num == 4:
+        goalpos = [1, 2]
+    if num == 5:
+        goalpos = [2, 2]
+    if num == 6:
+        goalpos = [2, 1]
+    if num == 7:
+        goalpos = [2, 0]
+    if num == 8:
+        goalpos = [1, 0]
+
+    distance = abs(numpos[0] - goalpos[0]) + abs(numpos[1] - goalpos[1])
+    return distance
+
+def totalDistance():
+    sum = 0
+
+    sum += calculateDistance(0)
+    sum += calculateDistance(1)
+    sum += calculateDistance(2)
+    sum += calculateDistance(3)
+    sum += calculateDistance(4)
+    sum += calculateDistance(5)
+    sum += calculateDistance(6)
+    sum += calculateDistance(7)
+    sum += calculateDistance(8)
+
+    return sum
+
+
 #main
 def main():
     fillInit()
-    for i in init:
-        print(i)
+    for i in range(3):
+        print(str(init[i]) + " " + str(goal[i]))
+    print(calculateDistance(1))
 
 if __name__ == "__main__":
     main()
