475807b85a80ffbe1e1a1fa1232deb9b3f5f7a7c
jamesrivett <jrivett@students.kennesaw.edu>
2021-10-17 02:03:37 -0400

    Parameterized all functions

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import random
-import time
+import node
 
-#globals
+# globals
 goal = [[1, 2, 3],[8, 0, 4],[7, 6, 5]]
 init = [[1, 2, 3],[8, 0, 4],[7, 6, 5]]
 
-#print current state and goal position
-def printBoard():
+# print current state and goal position
+def printBoard(state):
     print("Current:    Goal:")
     for i in range(3):
-        print(str(init[i]) + " | " + str(goal[i]))
+        print(str(state[i]) + " | " + str(goal[i]))
 
-#fills init list with distinct random values from 0 to 8
+# fills init list with distinct random values from 0 to 8
 def fillInit():
     used = []
     for i in range(3):
@@ -30,36 +30,36 @@ def fillInit():
                 used.append(num)
                 break
 
-def scramble(amt):
+def scramble(amt, state):
     print("Scrambling board by " + str(amt) + " random moves")
     for i in range(amt):
         move = random.randint(0, 3)
 
         if move == 0:
-            moveSuccess = up()
+            moveSuccess = up(state)
             if not moveSuccess: i += 1
         if move == 1:
-            moveSuccess = down()
+            moveSuccess = down(state)
             if not moveSuccess: i += 1
         if move == 2:
-            moveSuccess = left()
+            moveSuccess = left(state)
             if not moveSuccess: i += 1
         if move == 3:
-            moveSuccess = right()
+            moveSuccess = right(state)
             if not moveSuccess: i += 1
 
-#calculates num's distance to its goal position
-def calculateDistance(num):
+# calculates num's distance to its goal position
+def calculateDistance(num, state):
     numpos = []
     goalpos = []
 
-    #calculate num's position in init array
+    # calculate num's position in state array
     for i in range(3):
         for j in range(3):
-            if init[i][j] == num:
+            if state[i][j] == num:
                 numpos = [i, j]
 
-    #switch statement for goal positions
+    # switch statement for goal positions
     if num == 0:
         goalpos = [1, 1]
     if num == 1:
@@ -82,47 +82,47 @@ def calculateDistance(num):
     distance = abs(numpos[0] - goalpos[0]) + abs(numpos[1] - goalpos[1])
     return distance
 
-#calculate distance of all nums out of place
-def totalDistance():
+# calculate distance of all nums out of place
+def totalDistance(state):
     sum = 0
 
-    sum += calculateDistance(1)
-    sum += calculateDistance(2)
-    sum += calculateDistance(3)
-    sum += calculateDistance(4)
-    sum += calculateDistance(5)
-    sum += calculateDistance(6)
-    sum += calculateDistance(7)
-    sum += calculateDistance(8)
+    sum += calculateDistance(1, state)
+    sum += calculateDistance(2, state)
+    sum += calculateDistance(3, state)
+    sum += calculateDistance(4, state)
+    sum += calculateDistance(5, state)
+    sum += calculateDistance(6, state)
+    sum += calculateDistance(7, state)
+    sum += calculateDistance(8, state)
 
     return sum
 
-#returns false until init matches goal state
-def goalState():
+# returns false until state matches goal state
+def goalState(state):
     isGoalState = True
     for i in range(3):
         for j in range(3):
-            if init[i][j] != goal [i][j]:
+            if state[i][j] != goal [i][j]:
                 isGoalState = False
     
     return isGoalState
 
-#find position of zero
-def findZero():
+# find position of zero
+def findZero(state):
     for i in range(3):
         for j in range(3):
-            if init[i][j] == 0:
+            if state[i][j] == 0:
                 zeropos = [i, j]
     return zeropos
 
-#calculate possible moves
-def possMoves(last):
+# calculate possible moves
+def possMoves(last, state):
     zeropos = []
     moves = []
-    zeropos = findZero()
+    zeropos = findZero(state)
     print("last move: " + last)
 
-    #calculate possible vertical moves
+    # calculate possible vertical moves
     if zeropos[0] == 0 and last != "up":
         moves.append("down")
 
@@ -136,7 +136,7 @@ def possMoves(last):
         moves.append("up")
 
 
-    #calculate possible horizontal moves
+    # calculate possible horizontal moves
     if zeropos[1] == 0 and last != "left":
         moves.append("right")
 
@@ -152,97 +152,99 @@ def possMoves(last):
     print("possible moves: " + str(moves))
     return moves
 
-#calculate costs for moves
-def moveCost(moves):
+# calculate costs for moves
+def moveCost(moves, state):
     costs = [[] for r in range(len(moves))]
     for i in range(len(moves)):
         if moves[i] == "up":
-            up()
+            up(state)
             costs[i].append("up")
-            costs[i].append(totalDistance())
-            down()
+            costs[i].append(totalDistance(state))
+            down(state)
 
         if moves[i] == "down":
-            down()
+            down(state)
             costs[i].append("down")
-            costs[i].append(totalDistance())
-            up()
+            costs[i].append(totalDistance(state))
+            up(state)
         if moves[i] == "left":
-            left()
+            left(state)
             costs[i].append("left")
-            costs[i].append(totalDistance())
-            right()
+            costs[i].append(totalDistance(state))
+            right(state)
 
         if moves[i] == "right":
-            right()
+            right(state)
             costs[i].append("right")
-            costs[i].append(totalDistance())
-            left()
+            costs[i].append(totalDistance(state))
+            left(state)
 
     return costs
 
-#move functions {
-def up():
-    zeropos = findZero()
+# Make children 
+
+# move functions ---------------------------------
+def up(state):
+    zeropos = findZero(state)
     row = zeropos[0]
     col = zeropos[1]
     if row == 0:
         print("can't move up from here!")
         return False
 
-    temp = init[row - 1][col]
-    init[row - 1][col] = init[row][col]
-    init[row][col] = temp
+    temp = state[row - 1][col]
+    state[row - 1][col] = state[row][col]
+    state[row][col] = temp
     return True
 
-def down():
-    zeropos = findZero()
+def down(state):
+    zeropos = findZero(state)
     row = zeropos[0]
     col = zeropos[1]
     if row == 2:
         print("can't move down from here!")
         return False
 
-    temp = init[row + 1][col]
-    init[row + 1][col] = init[row][col]
-    init[row][col] = temp
+    temp = state[row + 1][col]
+    state[row + 1][col] = state[row][col]
+    state[row][col] = temp
     return True
     
-def left():
-    zeropos = findZero()
+def left(state):
+    zeropos = findZero(state)
     row = zeropos[0]
     col = zeropos[1]
     if col == 0:
         print("can't move left from here!")
         return False
 
-    temp = init[row][col - 1]
-    init[row][col - 1] = init[row][col]
-    init[row][col] = temp
+    temp = state[row][col - 1]
+    state[row][col - 1] = state[row][col]
+    state[row][col] = temp
     return True
 
-def right():
-    zeropos = findZero()
+def right(state):
+    zeropos = findZero(state)
     row = zeropos[0]
     col = zeropos[1]
     if col == 2:
         print("can't move right from here!")
         return False
 
-    temp = init[row][col + 1]
-    init[row][col + 1] = init[row][col]
-    init[row][col] = temp
+    temp = state[row][col + 1]
+    state[row][col + 1] = state[row][col]
+    state[row][col] = temp
     return True
-# }
+# ------------------------------------------------
 
-#Best First Search Function | Each funcion call is one step
-def bfs(last):
-    printBoard()
+# Best First Search Function | Each funcion call is one step
+def bfs(last, state):
+    printBoard(state)
     lowestCost = 100
     lowestMove = "none"
-    costs = moveCost(possMoves(last))
+    costs = moveCost(possMoves(last, state), state)
 
-    #find move with lowest cost
+    # find move with lowest cost
     for i in range(len(costs)):
         if costs[i][1] < lowestCost:
             lowestCost = costs[i][1]
@@ -250,34 +252,39 @@ def bfs(last):
 
     print("moving to: " + lowestMove)
 
-    #make move with lowest cost
+    # make move with lowest cost
     if lowestMove == "up":
-        up()
+        up(state)
         return "up"
     if lowestMove == "down":
-        down()
+        down(state)
         return "down"
     if lowestMove == "left":
-        left()
+        left(state)
         return "left"
     if lowestMove == "right":
-        right()
+        right(state)
         return "right"
         
 
-#main
+# main
 def main():
     lastMove = ""
-    scramble(20)
+    scramble(20, init)
+    printBoard(init)
     print("BEGIN")
     print("-----------------------------------")
     totalMoves = 0
 
-    while not goalState():
-        lastMove = bfs(lastMove)
+    while not goalState(init):
+        lastMove = bfs(lastMove, init)
         totalMoves += 1
 
+    printBoard(init)
+    print("-----------------------------------")
     print("Finished! Total Moves: " + str(totalMoves))
+    print("-----------------------------------")
+    
 
 if __name__ == "__main__":
     main()
