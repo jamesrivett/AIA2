4a244cc2180885c695042c9d1ea05ab55e113049
James Rivett <jrivett@students.kennesaw.edu>
2021-10-21 17:30:12 -0400

    uhh

--- node.py
+++ node.py
@@ -9,14 +9,33 @@ class node:
     lastMove = ""
     pathTo = []
 
-    def __init__(self, state, last, path):
+    def __init__(self, state, path):
         self.state = state
         self.children = self.findChildren()
-        self.lastMove = last
-        if last == "":
+        
+        # append only self state if path is empty, path and self state if not
+        if path == "":
             self.pathTo.append(state)
         else:
             self.pathTo.append(path)
+            self.pathTo.append(state)
+
+        if path != "":
+            # find last move
+            zeroOfSelf = findZero(self.state)
+            zeroOfLast = findZero(self.pathTo[-1])
+            diff = [zeroOfSelf[0] - zeroOfLast[0], zeroOfSelf[1] - zeroOfLast[1]]
+
+            if diff == [1, 0]:
+                self.lastMove = "down"
+            if diff == [-1, 0]:
+                self.lastMove = "up"
+            if diff == [0, 1]:
+                self.lastMove = "right"
+            if diff == [0, -1]:
+                self.lastMove = "left"
+
+
 
     def getPathTo(self):
         return self.pathTo
