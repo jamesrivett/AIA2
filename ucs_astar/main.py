703e9bd952b1af757f71d008cdfd104be7a2d8fe
James Rivett <jrivett@students.kennesaw.edu>
2021-10-21 23:24:00 -0400

    Movement

--- ucs_astar/main.py
+++ ucs_astar/main.py
@@ -1,7 +1,20 @@
 import node
 
+
+
 def main():
-    pass
+    goal = node.node([[1, 2, 3],[8, 0, 4],[7, 6, 5]], [])
+    init = node.node([[1, 2, 3],[8, 0, 4],[7, 6, 5]], [])
+
+    print(init.getState())
+    init.right()
+    print(init.getState())
+
+
+def scramble(node):
+    node.getState()
+
+
 
 if __name__ == "__main__":
     main()
--- ucs_astar/node.py
+++ ucs_astar/node.py
@@ -2,12 +2,16 @@ class node:
     state = []
     cost = 0
     pathTo = []
+    zeroPos = []
 
     def __init__(self, state, path):
         self.state = state
         pathTo = path
         pathTo.append(self)
+        self.zeroPos = self.getZeroPos()
     
+
+
     def getState(self):
         return self.state
 
@@ -16,3 +20,60 @@ class node:
 
     def getPathTo(self):
         return self.pathTo
+
+    def getZeroPos(self):
+        for i in range(3):
+            for j in range(3):
+                if self.state[i][j] == 0:
+                    zeropos = [i, j]
+        return zeropos
+
+
+    # movement functions
+    def up(self):
+        row = self.zeroPos[0]
+        col = self.zeroPos[1]
+        if row == 0:
+            print("can't move up from here!")
+            return False
+        temp = self.state[row - 1][col]
+        self.state[row - 1][col] = self.state[row][col]
+        self.state[row][col] = temp
+    def down(self):
+        row = self.zeroPos[0]
+        col = self.zeroPos[1]
+        if row == 0:
+            print("can't move up from here!")
+            return False
+        temp = self.state[row + 1][col]
+        self.state[row + 1][col] = self.state[row][col]
+        self.state[row][col] = temp
+    def left(self):
+        row = self.zeroPos[0]
+        col = self.zeroPos[1]
+        if row == 0:
+            print("can't move up from here!")
+            return False
+        temp = self.state[row][col - 1]
+        self.state[row][col - 1] = self.state[row][col]
+        self.state[row][col] = temp
+    def right(self):
+        row = self.zeroPos[0]
+        col = self.zeroPos[1]
+        if row == 0:
+            print("can't move up from here!")
+            return False
+        temp = self.state[row][col + 1]
+        self.state[row][col + 1] = self.state[row][col]
+        self.state[row][col] = temp
+
+
+    # returns result of movement functions
+    def geUp(self):
+        pass
+    def getDown(self):
+        pass
+    def getLeft(self):
+        pass
+    def getRight(self):
+        pass
